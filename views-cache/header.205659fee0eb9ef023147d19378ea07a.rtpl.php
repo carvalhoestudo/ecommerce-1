3dfc0648145b3c9e30de7652485d9785083f1725
carvalhoestudo <carvalho.madureira@hotmail.com>
2019-06-27 22:11:52 -0300

    Criando a Classe Page

---
+++ views-cache/header.205659fee0eb9ef023147d19378ea07a.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!doctype html>
+<html lang="pt_br">
+	<head>
+		<meta charset="utf-8">
+		<title>Ecommerce Teste</title>
+	</head>
+	<body>
+		
---
+++ views-cache/index.205659fee0eb9ef023147d19378ea07a.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1> Você está na Home da Loja</h1>
