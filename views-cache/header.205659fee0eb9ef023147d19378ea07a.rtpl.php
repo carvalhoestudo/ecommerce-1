b49d2ec6fe04c0041850b355e4952855f7376195
carvalhoestudo <carvalho.madureira@hotmail.com>
2019-07-01 22:22:11 -0300

    Criando a Classe Page

---
+++ views-cache/header.205659fee0eb9ef023147d19378ea07a.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt_BR">
+	<header>
+		<meta charset="UTF-8">
+		<title>Loja Carvalho TI</title>
+	</header>
+	<body>
+		
---
+++ views-cache/index.205659fee0eb9ef023147d19378ea07a.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Você está Na Home da Loja.</h1>
